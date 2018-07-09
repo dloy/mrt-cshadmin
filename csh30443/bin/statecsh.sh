a9d6f646ec51280c11860c72526f8ce9022a75e0
replic <replic@uc3-mrtreplic2-dev.cdlib.org>
2018-07-09 13:51:33 -0700

    Add exit code

--- csh30443/bin/statecsh.sh
+++ csh30443/bin/statecsh.sh
@@ -5,9 +5,12 @@ CMD="curl -ks -X GET https://localhost:30443/cloudhost/state/8100?t=xml"
 #printf "\n\n***CMD: $CMD\n"
 state=`$CMD`
 
-#Return shell message
+#Return shell message and status
+final_status=1
 if [[ $state != *"<chsvc:ok>true</chsvc:ok>"* ]]; then
   echo "FAIL:$state"
 else
   echo 'OK'
+  final_status=0
 fi
+exit $final_status
